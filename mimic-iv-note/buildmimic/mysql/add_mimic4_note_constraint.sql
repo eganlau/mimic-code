768e435ca8e6953a893f1e47336070ab554dc712
Your Name <you@example.com>
2024-06-01 00:57:53 +0800

    add mimic4 note constraints.sql

---
+++ mimic-iv-note/buildmimic/mysql/add_mimic4_note_constraint.sql
@@ -0,0 +1,63 @@
+use mimiciv;
+
+DROP PROCEDURE IF EXISTS PROC_DROP_FOREIGN_KEY;
+DELIMITER $$
+CREATE PROCEDURE PROC_DROP_FOREIGN_KEY(IN tableName VARCHAR(64), IN constraintName VARCHAR(64))
+BEGIN
+  IF EXISTS(
+    SELECT * FROM information_schema.table_constraints
+    WHERE 
+      table_schema    = DATABASE()     AND
+      table_name      = tableName      AND
+      constraint_name = constraintName AND
+      constraint_type = 'FOREIGN KEY')
+  THEN
+    SET @query = CONCAT('ALTER TABLE ', tableName, ' DROP FOREIGN KEY ', constraintName, ';');
+    PREPARE stmt FROM @query; 
+    EXECUTE stmt; 
+    DEALLOCATE PREPARE stmt; 
+  END IF; 
+END$$
+DELIMITER ;
+
+-- subject_id
+CALL PROC_DROP_FOREIGN_KEY('discharge', 'discharge_fk_subject_id');
+ALTER TABLE discharge
+ADD CONSTRAINT discharge_fk_subject_id
+  FOREIGN KEY (SUBJECT_ID)
+  REFERENCES PATIENTS(SUBJECT_ID);
+
+-- hadm_ids
+CALL PROC_DROP_FOREIGN_KEY('discharge', 'discharge_fk_hadm_id');
+ALTER TABLE discharge
+ADD CONSTRAINT discharge_fk_hadm_id
+  FOREIGN KEY (HADM_ID)
+  REFERENCES ADMISSIONS(HADM_ID);
+
+-- subject_id
+CALL PROC_DROP_FOREIGN_KEY('discharge_detail', 'discharge_detail_fk_subject_id');
+ALTER TABLE discharge_detail
+ADD CONSTRAINT discharge_detail_fk_subject_id
+  FOREIGN KEY (SUBJECT_ID)
+  REFERENCES PATIENTS(SUBJECT_ID);
+
+-- subject_id
+CALL PROC_DROP_FOREIGN_KEY('radiology', 'radiology_fk_subject_id');
+ALTER TABLE radiology
+ADD CONSTRAINT radiology_fk_subject_id
+  FOREIGN KEY (SUBJECT_ID)
+  REFERENCES PATIENTS(SUBJECT_ID);
+
+-- hadm_ids
+CALL PROC_DROP_FOREIGN_KEY('radiology', 'radiology_fk_hadm_id');
+ALTER TABLE radiology
+ADD CONSTRAINT radiology_fk_hadm_id
+  FOREIGN KEY (HADM_ID)
+  REFERENCES ADMISSIONS(HADM_ID);  
+
+-- subject_id
+CALL PROC_DROP_FOREIGN_KEY('radiology_detail', 'radiology_detail_fk_subject_id');
+ALTER TABLE radiology_detail
+ADD CONSTRAINT radiology_detail_fk_subject_id
+  FOREIGN KEY (SUBJECT_ID)
+  REFERENCES PATIENTS(SUBJECT_ID);

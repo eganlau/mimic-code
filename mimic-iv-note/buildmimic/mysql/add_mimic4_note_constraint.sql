7588d456586f02555cbfb0819ad72dfa2196b050
Your Name <you@example.com>
2024-06-03 14:15:06 +0800

    update the mysql load and index scripts

--- mimic-iv-note/buildmimic/mysql/add_mimic4_note_constraint.sql
+++ mimic-iv-note/buildmimic/mysql/add_mimic4_note_constraint.sql
@@ -24,40 +24,40 @@ DELIMITER ;
 CALL PROC_DROP_FOREIGN_KEY('discharge', 'discharge_fk_subject_id');
 ALTER TABLE discharge
 ADD CONSTRAINT discharge_fk_subject_id
-  FOREIGN KEY (SUBJECT_ID)
-  REFERENCES PATIENTS(SUBJECT_ID);
+  FOREIGN KEY (subject_id)
+  REFERENCES patients(subject_id);
 
 -- hadm_ids
 CALL PROC_DROP_FOREIGN_KEY('discharge', 'discharge_fk_hadm_id');
 ALTER TABLE discharge
 ADD CONSTRAINT discharge_fk_hadm_id
-  FOREIGN KEY (HADM_ID)
-  REFERENCES ADMISSIONS(HADM_ID);
+  FOREIGN KEY (hadm_id)
+  REFERENCES admissions(hadm_id);
 
 -- subject_id
 CALL PROC_DROP_FOREIGN_KEY('discharge_detail', 'discharge_detail_fk_subject_id');
 ALTER TABLE discharge_detail
 ADD CONSTRAINT discharge_detail_fk_subject_id
-  FOREIGN KEY (SUBJECT_ID)
-  REFERENCES PATIENTS(SUBJECT_ID);
-
+  FOREIGN KEY (subject_id)
+  REFERENCES patients(subject_id);
+  
 -- subject_id
 CALL PROC_DROP_FOREIGN_KEY('radiology', 'radiology_fk_subject_id');
 ALTER TABLE radiology
 ADD CONSTRAINT radiology_fk_subject_id
-  FOREIGN KEY (SUBJECT_ID)
-  REFERENCES PATIENTS(SUBJECT_ID);
+  FOREIGN KEY (subject_id)
+  REFERENCES patients(subject_id);
 
 -- hadm_ids
 CALL PROC_DROP_FOREIGN_KEY('radiology', 'radiology_fk_hadm_id');
 ALTER TABLE radiology
 ADD CONSTRAINT radiology_fk_hadm_id
-  FOREIGN KEY (HADM_ID)
-  REFERENCES ADMISSIONS(HADM_ID);  
+  FOREIGN KEY (hadm_id)
+  REFERENCES admissions(hadm_id);
 
 -- subject_id
 CALL PROC_DROP_FOREIGN_KEY('radiology_detail', 'radiology_detail_fk_subject_id');
 ALTER TABLE radiology_detail
 ADD CONSTRAINT radiology_detail_fk_subject_id
-  FOREIGN KEY (SUBJECT_ID)
-  REFERENCES PATIENTS(SUBJECT_ID);
+  FOREIGN KEY (subject_id)
+  REFERENCES patients(subject_id);
--- mimic-iv-note/buildmimic/mysql/load_mimic4_note.sql
+++ mimic-iv-note/buildmimic/mysql/load_mimic4_note.sql
@@ -1,17 +1,17 @@
--- For mimiciv_note.discharge table
-DROP TABLE IF EXISTS mimiciv_note.discharge;
-CREATE TABLE mimiciv_note.discharge
+-- For mimiciv.discharge table
+DROP TABLE IF EXISTS mimiciv.discharge;
+CREATE TABLE mimiciv.discharge
 (
   note_id VARCHAR(25) NOT NULL,
   subject_id INTEGER NOT NULL,
   hadm_id INTEGER NOT NULL,
   note_type VARCHAR(2) NOT NULL,
   note_seq SMALLINT NOT NULL,
-  charttime TIMESTAMP NOT NULL,
-  storetime TIMESTAMP,
+  charttime DATETIME NOT NULL,
+  storetime DATETIME,
   text TEXT NOT NULL
 );
-LOAD DATA LOCAL INFILE 'discharge.csv' INTO TABLE mimiciv_note.discharge
+LOAD DATA LOCAL INFILE 'discharge.csv' INTO TABLE mimiciv.discharge
 FIELDS TERMINATED BY ',' ESCAPED BY '' OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
@@ -22,24 +22,24 @@ subject_id = trim(@subject_id),
 hadm_id = IF(@hadm_id='', NULL, trim(@hadm_id)),
 note_type = trim(@note_type),
 note_seq = trim(@note_seq),
-charttime = trim(@charttime),
-storetime = IF(@storetime='', NULL, trim(@storetime)),
+charttime = @charttime,
+storetime = IF(@storetime='', NULL, @storetime),
 text = trim(@text);
 
--- For mimiciv_note.radiology table
-DROP TABLE IF EXISTS mimiciv_note.radiology;
-CREATE TABLE mimiciv_note.radiology
+-- For mimiciv.radiology table
+DROP TABLE IF EXISTS mimiciv.radiology;
+CREATE TABLE mimiciv.radiology
 (
   note_id VARCHAR(25) NOT NULL,
   subject_id INTEGER NOT NULL,
   hadm_id INTEGER,
   note_type VARCHAR(2) NOT NULL,
   note_seq SMALLINT NOT NULL,
-  charttime TIMESTAMP NOT NULL,
-  storetime TIMESTAMP,
+  charttime DATETIME NOT NULL,
+  storetime DATETIME,
   text TEXT NOT NULL
 );
-LOAD DATA LOCAL INFILE 'radiology.csv' INTO TABLE mimiciv_note.radiology
+LOAD DATA LOCAL INFILE 'radiology.csv' INTO TABLE mimiciv.radiology
 FIELDS TERMINATED BY ',' ESCAPED BY '' OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
@@ -50,13 +50,13 @@ subject_id = trim(@subject_id),
 hadm_id = IF(@hadm_id='', NULL, trim(@hadm_id)),
 note_type = trim(@note_type),
 note_seq = trim(@note_seq),
-charttime = trim(@charttime),
-storetime = IF(@storetime='', NULL, trim(@storetime)),
+charttime = @charttime,
+storetime = IF(@storetime='', NULL, @storetime),
 text = trim(@text);
 
--- For mimiciv_note.discharge_detail table
-DROP TABLE IF EXISTS mimiciv_note.discharge_detail;
-CREATE TABLE mimiciv_note.discharge_detail
+-- For mimiciv.discharge_detail table
+DROP TABLE IF EXISTS mimiciv.discharge_detail;
+CREATE TABLE mimiciv.discharge_detail
 (
   note_id VARCHAR(25) NOT NULL,
   subject_id INTEGER NOT NULL,
@@ -64,7 +64,7 @@ CREATE TABLE mimiciv_note.discharge_detail
   field_value TEXT NOT NULL,
   field_ordinal INTEGER NOT NULL
 );
-LOAD DATA LOCAL INFILE 'discharge_detail.csv' INTO TABLE mimiciv_note.discharge_detail
+LOAD DATA LOCAL INFILE 'discharge_detail.csv' INTO TABLE mimiciv.discharge_detail
 FIELDS TERMINATED BY ',' ESCAPED BY '' OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
@@ -76,9 +76,9 @@ field_name = trim(@field_name),
 field_value = trim(@field_value),
 field_ordinal = trim(@field_ordinal);
 
--- For mimiciv_note.radiology_detail table
-DROP TABLE IF EXISTS mimiciv_note.radiology_detail;
-CREATE TABLE mimiciv_note.radiology_detail
+-- For mimiciv.radiology_detail table
+DROP TABLE IF EXISTS mimiciv.radiology_detail;
+CREATE TABLE mimiciv.radiology_detail
 (
   note_id VARCHAR(25) NOT NULL,
   subject_id INTEGER NOT NULL,
@@ -86,7 +86,7 @@ CREATE TABLE mimiciv_note.radiology_detail
   field_value TEXT NOT NULL,
   field_ordinal INTEGER NOT NULL
 );
-LOAD DATA LOCAL INFILE 'radiology_detail.csv' INTO TABLE mimiciv_note.radiology_detail
+LOAD DATA LOCAL INFILE 'radiology_detail.csv' INTO TABLE mimiciv.radiology_detail
 FIELDS TERMINATED BY ',' ESCAPED BY '' OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
